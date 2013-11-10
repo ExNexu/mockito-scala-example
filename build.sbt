fbe5cc8b19919abd42a85d60c158014ff4d3b4f6
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2013-11-10 15:53:20 +1300

    Added some function examples

--- build.sbt
+++ build.sbt
@@ -6,9 +6,14 @@ version := "0.1"
 
 scalaVersion := "2.10.3"
 
+resolvers ++= Seq(
+  "Sonatype Releases" at "http://oss.sonatype.org/content/repositories/releases"
+)
+
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "1.9.2" % "test",
-  "org.mockito" % "mockito-all" % "1.9.5" % "test"
+  "org.mockito" % "mockito-all" % "1.9.5" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.10.0" % "test"
 )
 
 initialCommands := "import bleibinhaus.mockitoscalaexample._"
---
+++ src/test/scala/FunctionsTest.scala
@@ -0,0 +1,132 @@
+package bleibinhaus.mockitoscalaexample
+
+import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.any
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.when
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.prop.PropertyChecks
+
+class FunctionProvider {
+  // Int ⇒ Int == Function1[Int, Int]
+  def addingFunction(addend: Int): Int ⇒ Int =
+    (i: Int) ⇒ i + addend
+
+  // (Int, String) ⇒ Seq[String] == Function2[Int, String, Seq[String]]
+  def textNTimesFunction: (Int, String) ⇒ Seq[String] =
+    (times: Int, text: String) ⇒ 1 to times map { _ ⇒ text }
+
+  def applies23ToFunction(intFunction: Int ⇒ Int): Int =
+    intFunction(23)
+
+  def sumCurried(addend1: Int)(addend2: Int): Int =
+    addend1 + addend2
+
+  // Int ⇒ String ⇒ T[String] == Function1[Int, Function1[String, T[String]]]
+  def textNTimesAsXCurried[T[String]](
+    stringSeqTransformer: Seq[String] ⇒ T[String] = (seq: Seq[String]) ⇒ seq): Int ⇒ String ⇒ T[String] =
+    (times: Int) ⇒ (text: String) ⇒ stringSeqTransformer(textNTimesFunction(times, text))
+}
+
+class FunctionUser(val functionProvider: FunctionProvider) {
+  def do1And1 = functionProvider.addingFunction(1)(1)
+
+  def batman = functionProvider.textNTimesFunction(16, "na") :+ "batman"
+
+  def answer = {
+    val addToNineteenFunction: Int ⇒ Int = functionProvider.sumCurried(19) _
+    functionProvider.applies23ToFunction(addToNineteenFunction)
+  }
+
+  def ballmer = {
+    val multiplyTextNTimesAsListCurried = functionProvider.textNTimesAsXCurried {
+      (seq: Seq[String]) ⇒ seq.toList
+    }
+    val text14TimesAsList = multiplyTextNTimesAsListCurried(14)
+    text14TimesAsList("developers")
+  }
+}
+
+class FunctionsTest extends FunSpec with ShouldMatchers with MockitoSugar with PropertyChecks {
+  describe("The function provider") {
+    it("has a method which returns a function which adds a number to a predefined number") {
+      val functionProvider = new FunctionProvider
+      val addingFunction = functionProvider.addingFunction(23)
+      forAll { (n: Int) ⇒
+        addingFunction(n) should be(23 + n)
+      }
+    }
+
+    it("has a method which returns a function which multiplies a text n times") {
+      val functionProvider = new FunctionProvider
+      val textNTimesFunction = functionProvider.textNTimesFunction
+      textNTimesFunction(2, "text") should be(Seq("text", "text"))
+    }
+  }
+
+  describe("The function user") {
+    it("can do 1&1") {
+      val functionProviderMock = mock[FunctionProvider]
+      when(functionProviderMock.addingFunction(1)).thenReturn((i: Int) ⇒ i + 1)
+      val functionUser = new FunctionUser(functionProviderMock)
+      functionUser.do1And1 should be(2)
+      verify(functionProviderMock).addingFunction(1)
+    }
+
+    it("knows about batman") {
+      val functionProviderMock = mock[FunctionProvider]
+      val textNTimesFunctionMock = mock[Function2[Int, String, Seq[String]]]
+      when(functionProviderMock.textNTimesFunction).thenReturn(textNTimesFunctionMock)
+      when(textNTimesFunctionMock.apply(16, "na")).thenReturn(1 to 16 map { _ ⇒ "na" })
+
+      val functionUser = new FunctionUser(functionProviderMock)
+      functionUser.batman should be {
+        (1 to 16 map { _ ⇒ "na" }) :+ "batman"
+      }
+
+      verify(functionProviderMock).textNTimesFunction
+      verify(textNTimesFunctionMock).apply(16, "na")
+    }
+
+    it("can calculate the answer (just with mocks)") {
+      val functionProviderMock = mock[FunctionProvider]
+      when(functionProviderMock.applies23ToFunction(any[Function1[Int, Int]].apply)).thenReturn(42)
+
+      val functionUser = new FunctionUser(functionProviderMock)
+      functionUser.answer should be(42)
+
+      val nineteenFunctionCaptor = ArgumentCaptor.forClass(classOf[Function1[Int, Int]])
+      verify(functionProviderMock).applies23ToFunction(nineteenFunctionCaptor.capture.apply)
+      when(functionProviderMock.sumCurried(19)(0)).thenReturn(19)
+      when(functionProviderMock.sumCurried(19)(1)).thenReturn(20)
+      nineteenFunctionCaptor.getValue.apply(0) should be(19)
+      nineteenFunctionCaptor.getValue.apply(1) should be(20)
+      verify(functionProviderMock).sumCurried(19)(0)
+      verify(functionProviderMock).sumCurried(19)(1)
+    }
+
+    it("can calculate the answer (with a spy)") {
+      val functionProvider = new FunctionProvider
+      val functionProviderSpy = spy(functionProvider)
+      doReturn(42).when(functionProviderSpy).sumCurried(19)(23)
+
+      val functionUser = new FunctionUser(functionProviderSpy)
+      functionUser.answer should be(42)
+
+      verify(functionProviderSpy).sumCurried(19)(23)
+      val nineteenFunctionCaptor = ArgumentCaptor.forClass(classOf[Function1[Int, Int]])
+      verify(functionProviderSpy).applies23ToFunction(nineteenFunctionCaptor.capture.apply)
+      forAll { (n: Int) ⇒
+        nineteenFunctionCaptor.getValue.apply(n) should be(19 + n)
+      }
+    }
+
+    it("likes developers") {
+      // TODO
+    }
+  }
+}
